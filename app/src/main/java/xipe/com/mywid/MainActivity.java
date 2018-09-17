8bc2d62269872109eaf565a7f294bc9ac6ce4f73
fattahaabdul <fattah@xipetech.com>
2018-09-17 12:58:00 +0530

    Initial commit of top widget on 17 september 2018.

---
+++ app/src/main/java/xipe/com/mywid/MainActivity.java
@@ -0,0 +1,82 @@
+package xipe.com.mywid;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject").build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
+
+        //Check if the application has draw over other apps permission or not?
+        //This permission is by default available for API<23. But for API > 23
+        //you have to ask for the permission in runtime.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+        } else {
+            initializeView();
+        }
+    }
+
+    /**
+     * Set and initialize the view elements.
+     */
+    private void initializeView() {
+        findViewById(R.id.notify_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, ChatHeadService.class));
+                finish();
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+
+            //Check if the permission is granted or not.
+            // Settings activity never returns proper value so instead check with following method
+            if (Settings.canDrawOverlays(this)) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this,
+                        "Draw over other app permission not available. Closing the application",
+                        Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ app/src/main/java/xipe/com/mywid/OverlayShowingService.java
@@ -0,0 +1,132 @@
+package xipe.com.mywid;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class OverlayShowingService extends Service implements OnTouchListener, OnClickListener {
+    
+    private View topLeftView;
+
+    private Button overlayedButton;
+    private float offsetX;
+    private float offsetY;
+    private int originalXPos;
+    private int originalYPos;
+    private boolean moving;
+    private WindowManager wm;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+	return null;
+    }
+
+    @Override
+    public void onCreate() {
+	super.onCreate();
+
+	wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+	overlayedButton = new Button(this);
+	overlayedButton.setText("Overlay button");
+	overlayedButton.setOnTouchListener(this);
+	overlayedButton.setAlpha(0.0f);
+	overlayedButton.setBackgroundColor(0x55fe4444);
+	overlayedButton.setOnClickListener(this);
+
+	LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, LayoutParams.TYPE_SYSTEM_ALERT, LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+	params.gravity = Gravity.LEFT | Gravity.TOP;
+	params.x = 0;
+	params.y = 0;
+	wm.addView(overlayedButton, params);
+
+	topLeftView = new View(this);
+	LayoutParams topLeftParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, LayoutParams.TYPE_SYSTEM_ALERT, LayoutParams.FLAG_NOT_FOCUSABLE | LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+	topLeftParams.gravity = Gravity.LEFT | Gravity.TOP;
+	topLeftParams.x = 0;
+	topLeftParams.y = 0;
+	topLeftParams.width = 0;
+	topLeftParams.height = 0;
+	wm.addView(topLeftView, topLeftParams);
+
+    }
+
+    @Override
+    public void onDestroy() {
+	super.onDestroy();
+	if (overlayedButton != null) {
+	    wm.removeView(overlayedButton);
+	    wm.removeView(topLeftView);
+	    overlayedButton = null;
+	    topLeftView = null;
+	}
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+	if (event.getAction() == MotionEvent.ACTION_DOWN) {
+	    float x = event.getRawX();
+	    float y = event.getRawY();
+
+	    moving = false;
+
+	    int[] location = new int[2];
+	    overlayedButton.getLocationOnScreen(location);
+
+	    originalXPos = location[0];
+	    originalYPos = location[1];
+
+	    offsetX = originalXPos - x;
+	    offsetY = originalYPos - y;
+
+	} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+	    int[] topLeftLocationOnScreen = new int[2];
+	    topLeftView.getLocationOnScreen(topLeftLocationOnScreen);
+
+	    System.out.println("topLeftY="+topLeftLocationOnScreen[1]);
+	    System.out.println("originalY="+originalYPos);
+	    
+	    float x = event.getRawX();
+	    float y = event.getRawY();
+
+	    LayoutParams params = (LayoutParams) overlayedButton.getLayoutParams();
+
+	    int newX = (int) (offsetX + x);
+	    int newY = (int) (offsetY + y);
+
+	    if (Math.abs(newX - originalXPos) < 1 && Math.abs(newY - originalYPos) < 1 && !moving) {
+		return false;
+	    }
+
+	    params.x = newX - (topLeftLocationOnScreen[0]);
+	    params.y = newY - (topLeftLocationOnScreen[1]);
+
+	    wm.updateViewLayout(overlayedButton, params);
+	    moving = true;
+	} else if (event.getAction() == MotionEvent.ACTION_UP) {
+	    if (moving) {
+		return true;
+	    }
+	}
+
+	return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+	Toast.makeText(this, "Overlay button click event", Toast.LENGTH_SHORT).show();
+    }
+
+}
